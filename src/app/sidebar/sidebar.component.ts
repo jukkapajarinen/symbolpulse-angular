813312be9f88db8ebb7b8e1df0bfe9e754910b42
Jukka Pajarinen <jukka.pajarinen@lumitum.com>
2023-12-07 16:54:04 +0200

    fix routing with routermodule

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MySymbolsService } from '../my-symbols.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidebar.component.html',
 })
 export class SidebarComponent implements OnInit {
